b50c242639a6cbff1ae50912fcadbf3104abf05f
kursiel <kevin.cascante.06@gmail.com>
2019-05-30 15:48:00 -0600

    First commit
    first commit

---
+++ TaskManagerAPI/Interfaces/IUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using TaskManagerAPI.Model;
+
+namespace TaskManagerAPI.Interfaces
+{
+    public interface IUser
+    {
+        IMongoCollection<User> Users { get; }
+    }
+}
---
+++ TaskManagerAPI/Model/User.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace TaskManagerAPI.Model
+{
+    public class User
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+        [BsonElement("Username")]
+        public string Username { get; set; }
+        [BsonElement("Email")]
+        public string Email { get; set; }
+        [BsonElement("Password")]
+        public string Password { get; set; }
+        [BsonElement("Name")]
+        public string  Name { get; set; }
+        [BsonElement("lastName")]
+        public string LastName { get; set; }
+        [BsonElement("Cedula")]
+        public string Cedula { get; set; }
+        [BsonElement("Team")]
+        public string Team { get; set; }
+        [BsonElement("EmployeeID")]
+        public string EmployeeId { get; set; }
+        [BsonElement("BornDate")]
+        public DateTime BornDate { get; set; }
+    }
+}
